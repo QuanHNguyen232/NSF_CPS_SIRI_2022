bd4d1b55b15f8190416eab874be14e348f5b839b
QuanHNguyen232 <quan.nh2002@gmail.com>
2022-07-12 17:08:51 -0400

    clean code

--- eye-track-data-process/convert-eyetracking-txt.py
+++ eye-track-data-process/convert-eyetracking-txt.py
@@ -1,25 +1,45 @@
+# %%
 import pandas as pd
-import numpy as np
 
-file_id = 'P2'
-folder = file_id + '/Eye Tracking/'
+# %%
+def modify_eye_trk(p_id):
+    file_id = 'P%d'%(p_id)
+    folder = file_id + '/Eye Tracking/'
 
-file_1 = file_id + '.txt'
-outname = file_id + '(modified).txt'
+    file_1 = file_id + '.txt'
+    outname = file_id + '-modified.txt'
 
-start = 3
-end = start + 9
+    start = 3
+    end = start + 10
 
-# read .miniSim file
-df = pd.read_csv(folder + file_1, sep='\t', dtype=str)
-# change eye-tracking col
-df['merge'] = df['System Time']
-df['merge'] = df['merge'].apply(lambda x : str(x)[start:end])
-
+    # read .miniSim file
+    df = pd.read_csv(folder + file_1, sep='\t', dtype=str)
+    # change eye-tracking col
+    df['System Time'] = df['System Time'].apply(lambda x : str(x)[start:end])
 
 
-# Save df as file
-df.to_csv(folder + outname, header=True, index=False, sep='\t', mode='a')
 
-print(df.head(5))
-print(df.shape)
+    # Save df as file
+    total_nan = df.isna().sum().sum()
+    if total_nan == 0:
+        df.to_csv(folder + outname, header=True, index=False, sep='\t', mode='a')
+    elif total_nan <= len(df.columns):
+        df.dropna(axis=0, inplace=True)
+        df.to_csv(folder + outname, header=True, index=False, sep='\t', mode='a')
+    else:
+        print('########### MISSING VALUES ###########')
+        return False
+        
+    print('modify_eye_trk func -- create {%s} -- DONE'%(outname))
+    return True
+
+# %%
+if __name__ == '__main__':
+
+    for i in range(30, 33):
+        
+        print('=== ID ==== P%d ==='%(i))
+        if not modify_eye_trk(i):
+            break
+    
+    print('convert-eyetracking-txt.py -- DONE')
--- eye-track-data-process/convert-miniSim-txt.py
+++ eye-track-data-process/convert-miniSim-txt.py
@@ -1,24 +1,42 @@
+# %%
 import pandas as pd
-import numpy as np
 
-file_id = 'P2'
-folder = file_id + '/Eye Tracking/'
+# %%
+def modify_miniSim(p_id):
 
-file_1 = file_id + '.miniSim'
-outname = file_id +'-miniSim(modified).txt'
+    file_id = 'P%d'%(p_id)
+    folder = file_id + '/Eye Tracking/'
 
-start = 4
-end = start + 9
+    file_1 = file_id + '.miniSim'
+    outname = file_id +'-miniSim-modified.txt'
 
-headers_name = ['Frames0', '?_time', 'merge']
+    start = 4
+    end = start + 10
 
-# read .miniSim file
-df_sim = pd.read_csv(folder + file_1, header=None, names=headers_name, dtype=str)
-# change eye-tracking col
-df_sim['merge'] = df_sim['merge'].apply(lambda x : str(x).replace('.', '')[start:end])
+    headers_name = ['Frames0', 'X', 'System Time']
 
-# Save df as file
-df_sim.to_csv(folder + outname, header=True, index=False, sep='\t', mode='a')
+    # read .miniSim file
+    df_sim = pd.read_csv(folder + file_1, header=None, names=headers_name, dtype=str)
+    # change eye-tracking col
+    df_sim['System Time'] = df_sim['System Time'].apply(lambda x : str(x).replace('.', '')[start:end])
 
-print(df_sim.head(5))
-print(df_sim.shape)
+    # Save df as file
+    if df_sim.isna().sum().sum() == 0:
+        df_sim.to_csv(folder + outname, header=True, index=False, sep='\t', mode='a')
+    else:
+        print('########### MISSING VALUES ###########')
+        return False
+    
+    print('modify_miniSim func -- create {%s} -- DONE'%(outname))
+    return True
+
+# %%
+if __name__ == '__main__':
+
+    for i in range(1, 33):
+        
+        print('=== ID ==== P%d ==='%(i))
+        if not modify_miniSim(i):
+            break
+
+    print('convert-miniSim-txt.py -- DONE')
